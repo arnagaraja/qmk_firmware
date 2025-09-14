96ab963bd305949b0f1f8ec13afadab1bfebc326
Arpun Nagaraja <arnagaraja@gmail.com>
2025-09-13 22:02:32 -0500

    Added mouse layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/arnagaraja/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/arnagaraja/keymap.c
@@ -3,9 +3,11 @@
 #define _QWERTY 0
 #define _NAV 1
 #define _SYM 2
+#define _MOUSE 3
 
 #define SYM MO(_SYM)
 #define NAV MO(_NAV)
+#define MOUSE MO(_MOUSE)
 
 #define KC_CAD LALT(LCTL(KC_DEL))
 #define KC_SWLT LWIN(LCTL(KC_LEFT))
@@ -20,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_GRV,KC_LCMD,                                                                KC_PLUS, KC_EQL,
                                          KC_SPC,KC_TAB,                                                          KC_BSPC, KC_ENT,
                                          _______,_______,                                         _______,  KC_ESC,
-                                         _______, _______,                                        _______, KC_CAD
+                                         _______, _______,                                        _______, TG(MOUSE)
     ),
 
     [_NAV] = LAYOUT_5x6(
@@ -37,11 +39,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYM] = LAYOUT_5x6(
           KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-          _______,_______,_______,S(KC_LBRC),S(KC_RBRC),_______,                        _______,_______,_______,_______ ,_______,_______,
-          _______,_______,_______,KC_LPRN,KC_RPRN,_______,                        _______,_______,_______,_______,_______,_______,
-          _______,_______,_______,KC_LBRC,KC_RBRC,_______,                        _______,_______,_______,_______,_______,_______,
+          _______,_______,_______,S(KC_LBRC),S(KC_RBRC),_______,                  _______,KC_7,KC_8,KC_9 ,_______,_______,
+          _______,_______,LSFT(KC_MINS),KC_LPRN,KC_RPRN,_______,                  _______,KC_4,KC_5,KC_6,_______,_______,
+          _______,_______,_______,KC_LBRC,KC_RBRC,_______,                        _______,KC_1,KC_2,KC_3,_______,_______,
                                                   _______,_______,            _______ ,_______,
+                                                  _______,_______,            _______,KC_0,
                                                   _______,_______,            _______,_______,
+                                                  _______,_______,            _______,_______
+    ),
+
+    [_MOUSE] = LAYOUT_5x6(
+          _______ , _______ , _______ , _______ , _______ , _______ ,        _______,_______,_______,_______ ,_______,_______,
+          KC_VOLU,_______,_______,MS_UP ,MS_WHLU,_______,                  _______,_______,_______,_______ ,_______,_______,
+          KC_VOLD,_______,MS_LEFT,MS_DOWN ,MS_RGHT,_______,                 _______,_______,_______,_______,_______,_______,
+         _______,_______,_______,_______ ,MS_WHLD,_______,                     _______,_______,_______,_______,_______,_______,
+                                                  _______,_______,            _______ ,_______,
+                                                  MS_BTN1,MS_BTN2,            _______,_______,
                                                   _______,_______,            _______,_______,
                                                   _______,_______,            _______,_______
     )
@@ -52,9 +65,11 @@ const uint16_t PROGMEM screenshot[] = {KC_W, KC_E, KC_R, COMBO_END}; // WINDOWS 
 const uint16_t PROGMEM cad[] = {KC_S  , LT(NAV,KC_D)  , KC_F, COMBO_END}; // CTRL + ALT + DEL
 const uint16_t PROGMEM teams_mute[] = {KC_X  , LCTL_T(KC_C)  , KC_V, COMBO_END}; // Mute MS Teams (Windows)
 const uint16_t PROGMEM caps_lock[] = {KC_LSFT  , KC_RSFT  , COMBO_END}; // Caps Lock
+const uint16_t PROGMEM mouse[] = {KC_GRV, KC_LCMD, COMBO_END}; // Caps Lock
 combo_t key_combos[] = {
     COMBO(screenshot, LWIN(LSFT(KC_S))),
     COMBO(cad, KC_CAD),
     COMBO(teams_mute, LCTL(LSFT(KC_M))),
     COMBO(caps_lock, KC_CAPS),
+    COMBO(mouse, TG(MOUSE)),
 };
